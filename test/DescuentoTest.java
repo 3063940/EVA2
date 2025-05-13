4195e29987356ca9309c2c6ed14f3100f37fee07
Maria Collado <3063940@alu.murciaeduca.es>
2025-05-13 12:01:49 +0200

    Ultimo Test Unitario

--- test/DescuentoTest.java
+++ test/DescuentoTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DescuentoTest {
-  
+
+    @Test
+    void testCalcularDescuento()  {
+        assertEquals(0, Descuento.calculateDiscount(4, 150, false));
+    }
+
+    @Test
+    void testDescuento180() {
+        assertEquals(180, Descuento.calculateDiscount(12,600,true));
+    }
+
+    @Test
+    void testDescuento45() {
+        assertEquals(45.0, Descuento.calculateDiscount(7,300,false),001);
+    }
+
+    @Test
+    void testDescuento150() {
+        assertEquals(150, Descuento.calculateDiscount(7,600,true));
+    }
+
+    @Test
+    void testDescuentoRevisandoEl30() {
+        assertEquals(3000, Descuento.calculateDiscount(12,10000,true));
+    }
 }
